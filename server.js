b17e6479db91d01ff3b48bb18ea49a996958930b
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-06-22 11:28:44 -0700

    [Add] Set up express handlebars for templating engine

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const exphbs  = require('express-handlebars')
 const express = require('express')
 const app = express()
 const port = 3000
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home', {
+    greeting: 'Hello, World!'
+  })
 })
 
 app.listen(port, () => {
